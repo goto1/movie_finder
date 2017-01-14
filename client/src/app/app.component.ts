f24d79d5c716bb25cd3677611e3ca50690ba4e5f
Tom <tgoralczyk1@gmail.com>
2017-01-14 11:18:06 -0500

    Moved the template for app.component inside

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,9 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <header>
+      <div class="logo header-item">MOVIEJO</div>
+      <nav class="header-item">
+        <button class="nav-item" *ngIf="!userLoggedIn">
+          Login <i class="fa fa-user" aria-hidden="true"></i>
+        </button>
+        <button class="nav-item" *ngIf="userLoggedIn">
+          Logout <i class="fa fa-sign-out" aria-hidden="true"></i>
+        </button>
+      </nav>
+    </header>
+  `,
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'app works!';
+  public userLoggedIn: boolean = false;
 }
