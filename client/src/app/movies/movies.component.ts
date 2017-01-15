b7fa191f27f62fef3225cbc2ec0cb9ca56f2106e
Tom <tgoralczyk1@gmail.com>
2017-01-14 19:39:17 -0500

    Added a routing module for the movies/home section

---
+++ client/src/app/movies/movies.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  template: `
+    <trending-movies></trending-movies>
+    <h1>Movies Component</h1>
+  `
+})
+export class MoviesComponent { }
