15a7dfa5ea46efdce135b2617aaf3d7353ed4a21
Tom <tgoralczyk1@gmail.com>
2017-01-16 09:50:26 -0500

    Added necessary dependencies to the movies module

--- client/src/app/movies/movies.component.ts
+++ client/src/app/movies/movies.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit }  from '@angular/core';
+
+import { MoviesService }      from './movies.service';
+import { Movie }              from './trending-movies/movie';
 
 @Component({
-  template: `
-    <trending-movies></trending-movies>
-    <h1>Movies Component</h1>
-  `
+  templateUrl: './movies.component.html',
+  styleUrls: [ './movies.component.sass' ]
 })
-export class MoviesComponent { }
+export class MoviesComponent implements OnInit { 
+  public movies: Movie[];
+  public errorMessage: string;
+  public searchQuery: string;
+
+  constructor(private moviesService: MoviesService) { }
+
+  ngOnInit(): void {
+    this.searchQuery = 'The Hangover';
+    this.searchMovie(this.searchQuery);
+  }
+
+  public searchMovie(title: string): void {
+    if (title) {
+      this.searchQuery = title;
+      this.moviesService.searchForMovie(title).subscribe(
+        movies => this.movies = movies,
+        error => this.errorMessage = <any>error
+      );
+    }
+  }
+}
--- client/src/app/movies/movies.module.ts
+++ client/src/app/movies/movies.module.ts
@@ -1,24 +1,35 @@
 import { NgModule }                 from '@angular/core';
 import { CommonModule }             from '@angular/common';
-import { HttpModule }               from '@angular/http';
+import { HttpModule, JsonpModule }  from '@angular/http';
+import { FormsModule }              from '@angular/forms';
 
 import { MoviesComponent }          from './movies.component';
 import { TrendingMoviesComponent }  from './trending-movies/trending-movies.component';
 import { MoviesRoutingModule }      from './movies-routing.module';
 import { TrendingMoviesService }    from './trending-movies/trending-movies.service';
+import { MoviesService }            from './movies.service';
+
+import { MovieTitlePipe }           from './trending-movies/movie-title.pipe';
+import { MovieTitleSearchPipe }     from './pipes/movie-title-search.pipe';
 
 @NgModule({
   imports: [
     CommonModule,
     HttpModule,
+    JsonpModule,
+    FormsModule,
     MoviesRoutingModule
   ],
   declarations: [
     MoviesComponent,
-    TrendingMoviesComponent
+    TrendingMoviesComponent,
+
+    MovieTitlePipe,
+    MovieTitleSearchPipe
   ],
   providers: [
-    TrendingMoviesService
+    TrendingMoviesService,
+    MoviesService
   ]
 })
 export class MoviesModule { }
