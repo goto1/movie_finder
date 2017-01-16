b06249c8d3c781ce89bfedd30fad5ce1ed2e2c1f
Tom <tgoralczyk1@gmail.com>
2017-01-16 16:07:46 -0500

    Refactored the search-movie-service for the search movie component

---
+++ client/src/app/movies/services/search-movies.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+import { MovieDBService } from './moviedb.service';
+import { Movie } from '../models/movie';
+import { API } from './moviedb-api-info';
+
+@Injectable()
+export class SearchMoviesService extends MovieDBService {
+  private apiUrl = API.url + '/search/movie?' + API.key + '&language=en-US&page=1&include_adult=false';
+
+  constructor(private http: Http) { super(); }
+
+  public searchMovie(title: string): Observable<Movie[]> {
+    title = title.replace(/\s/g, '%20');
+    this.apiUrl += '&query=' + title;
+    console.log(this.apiUrl);
+    return this.http.get(this.apiUrl)
+      .map(this.extractData)
+      .catch(this.handleError);
+  }
+}
