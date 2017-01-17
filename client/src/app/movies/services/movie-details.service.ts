0acdd1b59edf3f09b8450c1c59a84f42cff95b34
Tom <tgoralczyk1@gmail.com>
2017-01-16 19:09:02 -0500

    Implemented a service for retrieving movie details

---
+++ client/src/app/movies/services/movie-details.service.ts
@@ -0,0 +1,25 @@
+import { Injectable }     from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable }     from 'rxjs/Observable';
+
+import { MovieDBService } from './moviedb.service';
+import { Movie }          from '../models/movie';
+import { API }            from './moviedb-api-info';
+
+@Injectable()
+export class MovieDetailsService extends MovieDBService {
+
+  constructor(private http: Http) { super(); }
+
+  public getDetails(id: number | string): Observable<Movie> {
+    const apiUrl = API.url + '/movie/' + id + '?' + 
+      API.key + '&language=en-US' + '&append_to_response=videos,similar';
+    return this.http.get(apiUrl)
+      .map(this.extractData)
+      .catch(this.handleError);
+  }
+
+  protected extractData(res: Response) {
+    return res.json();
+  }
+}
