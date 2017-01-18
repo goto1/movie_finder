34180f8078d78515284a391af697bc24b3de422b
Tom <tgoralczyk1@gmail.com>
2017-01-18 12:06:37 -0500

    Changes to the detailed-movie type

--- client/src/app/movies/models/detailed-movie.ts
+++ client/src/app/movies/models/detailed-movie.ts
@@ -1,17 +1,13 @@
 import { Movie } from './movie';
 import { Genre } from './genre';
 
-export interface DetailedMovie {
-  id: number;
+export interface DetailedMovie extends Movie {
   imdb_id: string;
-  title: string;
   overview: string;
-  poster_path: string;
   release_date: string;
   runtime: number;
   similar: Movie[];
   trailer: string;
-  vote_average: number;
   vote_count: number;
   genres: Genre[];
 }
