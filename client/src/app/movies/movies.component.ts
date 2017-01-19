f41389f92b65b6d30af51cf0381f860f9e6e5066
Tom <tgoralczyk1@gmail.com>
2017-01-19 07:04:44 -0500

    Added and styled a navigation for the 'discover' and 'search' sections

--- client/src/app/movies/movies.component.ts
+++ client/src/app/movies/movies.component.ts
@@ -2,8 +2,17 @@ import { Component }  from '@angular/core';
 
 @Component({
   template: `
+    <nav>
+      <div class="nav-item">
+        Discover <i class="fa fa-film" aria-hidden="true"></i>
+      </div>
+      <div class="nav-item">
+        Search <i class="fa fa-search" aria-hidden="true"></i>
+      </div>
+    </nav>
     <movies-trending></movies-trending>
     <movies-search></movies-search>
-  `
+  `,
+  styleUrls: [ './movies.component.sass' ]
 })
 export class MoviesComponent { }
