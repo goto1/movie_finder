b072883607a8e438436b77d330e4abd6f5bd160c
Tom <tgoralczyk1@gmail.com>
2017-01-20 07:10:17 -0500

    Added a new attribute into the now-playing-component

--- client/src/app/movies/movies-discover/now-playing-movies.component.ts
+++ client/src/app/movies/movies-discover/now-playing-movies.component.ts
@@ -1,27 +1,39 @@
 import { Component, OnInit }        from '@angular/core';
 
 import { Movie }                    from '../models/movie';
-import { Pagination }               from './pagination';
+import { MoviesPaginated }          from './movies-paginated';
 import { DiscoverMoviesService }    from '../services/discover-movies.service';
 
 @Component({
   selector: 'now-playing-movies',
   templateUrl: './movies-discover.component.html',
-  styleUrls: [ './movies-discover.component.sass' ]
+  styleUrls: [ './movies-discover.component.sass' ],
 })
-export class NowPlayingMoviesComponent extends Pagination implements OnInit {
-  
-  constructor(protected discoverMoviesService: DiscoverMoviesService) {
-    super();
-  }
+export class NowPlayingMoviesComponent implements OnInit {
+  public showMovies: Movie[];
+  private moviesPaginated: MoviesPaginated;
+  public title: string = 'Now Playing';
+
+  constructor(private discoverMoviesService: DiscoverMoviesService) { }
 
   ngOnInit(): void {
     this.discoverMoviesService.getMovies().subscribe(
       movies => {
-        this.allMovies = movies;
-        this.showMovies = this.allMovies[this.page];
+        this.moviesPaginated = new MoviesPaginated(movies);
+        this.showMovies = this.moviesPaginated.getCurrentPageContent();
       },
       error => console.error(error)
     );
   }
+
+  public nextPage(): void {
+    this.moviesPaginated.nextPage();
+    this.showMovies = this.moviesPaginated.getCurrentPageContent();
+  }
+
+  public prevPage(): void {
+    this.moviesPaginated.previousPage();
+    this.showMovies = this.moviesPaginated.getCurrentPageContent();
+  }
 }
+
