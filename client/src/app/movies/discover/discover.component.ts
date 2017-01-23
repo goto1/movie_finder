a22c8ccf67ec5f7261a9391688901d513c6a8705
Tom <tgoralczyk1@gmail.com>
2017-01-23 08:55:37 -0500

    Changes to the discover page

---
+++ client/src/app/movies/discover/discover.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Category {
+  value: number;
+  name: string;
+};
+
+interface Selected {
+  category: Category;
+}
+
+@Component({
+  templateUrl: './discover.component.html',
+  styleUrls: [ './discover.component.sass' ]
+})
+export class DiscoverComponent implements OnInit {
+  public selected: Selected;
+  public categories: Category[] = [
+    { value: 1, name: 'Now Playing' },
+    { value: 2, name: 'Popular' },
+    { value: 3, name: 'Top Rated' },
+    { value: 4, name: 'Upcoming'}
+  ];
+  
+  ngOnInit(): void {
+    this.selected = {
+      category: this.categories[0]
+    };
+  }
+
+}
