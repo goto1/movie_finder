5be4e78aa9788f602b3ce7971d2bdff1ae58994b
Tom <tgoralczyk1@gmail.com>
2017-01-24 08:10:49 -0500

    Added a component for popular category

---
+++ client/src/app/movies/discover/popular.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit }  from '@angular/core';
+
+import { PopularService }     from '../services/popular.service';
+import { Movie }              from '../models/movie';
+
+@Component({
+  selector: 'popular',
+  templateUrl: './display-movies-template.html',
+  styleUrls: [ './display-movies-template.sass' ]
+})
+export class PopularComponent implements OnInit {
+  public movies: Movie[];
+  public error: boolean;
+
+  constructor(private popularService: PopularService) { }
+
+  ngOnInit(): void {
+    this.getMovies();
+  }
+
+  private getMovies(): void {
+    this.popularService.getPopular()
+      .subscribe(
+        movies => this.movies = movies,
+        error => this.error = error
+      );
+  }
+
+  public nextPage(): void {
+    this.popularService.nextPage();
+    this.getMovies();
+  }
+
+  public previousPAge(): void {
+    this.popularService.previousPage();
+    this.getMovies();
+  }
+
+  public hasPrevious(): boolean {
+    return this.popularService.hasPrevious();
+  }
+
+  public hasNext(): boolean {
+    return this.popularService.hasNext();
+  }
+
+}
