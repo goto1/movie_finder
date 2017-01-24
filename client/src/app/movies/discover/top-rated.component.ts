603c940f30f06bcb9640827cbb636a09e084db35
Tom <tgoralczyk1@gmail.com>
2017-01-24 08:07:05 -0500

    Created a component for top-rated category

---
+++ client/src/app/movies/discover/top-rated.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit }  from '@angular/core';
+
+import { TopRatedService }    from '../services/top-rated.service';
+import { Movie }              from '../models/movie';
+
+@Component({
+  selector: 'top-rated',
+  templateUrl: './display-movies-template.html',
+  styleUrls: [ './display-movies-template.sass' ]
+})
+export class TopRatedComponent implements OnInit {
+  public movies: Movie[];
+  public error: boolean;
+
+  constructor(private topRatedService: TopRatedService) { }
+
+  ngOnInit(): void {
+    this.getMovies();
+  }
+
+  private getMovies(): void {
+    this.topRatedService.getTopRated()
+      .subscribe(
+        movies => this.movies = movies,
+        error => this.error = true
+      )
+  }
+
+  public nextPage(): void {
+    this.topRatedService.nextPage();
+    this.getMovies();
+  }
+
+  public previousPage(): void {
+    this.topRatedService.previousPage();
+    this.getMovies();
+  }
+
+  public hasPrevious(): boolean {
+    return this.topRatedService.hasPrevious();
+  }
+
+  public hasNext(): boolean {
+    return this.topRatedService.hasNext();
+  }
+}
