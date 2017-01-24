e3bd9abb2b4f6d1882163673227f54d5ddd961c4
Tom <tgoralczyk1@gmail.com>
2017-01-24 08:23:15 -0500

    Refactored the discover component and move the template inside the component

--- client/src/app/movies/discover/discover.component.ts
+++ client/src/app/movies/discover/discover.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
-interface Category {
-  value: number;
-  name: string;
-};
-
-interface Selected {
-  category: Category;
-}
+import { Category } from '../models/category'
+import { Selected } from '../models/selected';
 
 @Component({
-  templateUrl: './discover.component.html',
+  template: `
+    <div class="select-category">
+      <div class="select-wrapper">
+        <select name="category" [(ngModel)]="selected.category">
+          <option *ngFor="let category of categories" [ngValue]="category">
+            {{category.name}}
+          </option>
+        </select>
+      </div>
+    </div>
+    <now-playing *ngIf="selected.category.value === 1"></now-playing>
+    <popular *ngIf="selected.category.value === 2"></popular>
+    <top-rated *ngIf="selected.category.value === 3"></top-rated>
+    <upcoming *ngIf="selected.category.value === 4"></upcoming>
+  `,
   styleUrls: [ './discover.component.sass' ]
 })
 export class DiscoverComponent implements OnInit {
@@ -27,5 +35,4 @@ export class DiscoverComponent implements OnInit {
       category: this.categories[0]
     };
   }
-
 }
