f05254d14b8a129b0f948e8f94db925dc5b95fff
Tom <tgoralczyk1@gmail.com>
2017-01-24 09:10:32 -0500

    Minor refactoring

--- client/src/app/movies/movies.module.ts
+++ client/src/app/movies/movies.module.ts
@@ -3,30 +3,31 @@ import { CommonModule, Location }     from '@angular/common';
 import { HttpModule }                 from '@angular/http';
 import { FormsModule }                from '@angular/forms';
 
+// Routing
 import { MoviesRoutingModule }        from './movies-routing.module';
 
+// Components
 import { MoviesComponent }            from './movies.component';
-import { MoviesSearchComponent }      from './movies-search/movies-search.component';
-import { SearchResultsComponent }     from './movies-search/search-results.component';
-import { MovieDetailsComponent }      from './movie-details/movie-details.component';
-
-import { SearchMoviesService }        from './services/search-movies.service';
-import { MovieDetailsService }        from './services/movie-details.service';
-
-import { MovieTitleShortPipe }        from './pipes/movie-title-short.pipe';
-import { SafeUrlPipe }                from './pipes/safe-url.pipe';
-
-// Refactoring
 import { DiscoverComponent }          from './discover/discover.component';
 import { NowPlayingComponent }        from './discover/now-playing.component';
 import { PopularComponent }           from './discover/popular.component';
 import { TopRatedComponent }          from './discover/top-rated.component';
 import { UpcomingComponent }          from './discover/upcoming.component';
-import { DiscoverService }            from './services/discover.service';
+import { MoviesSearchComponent }      from './movies-search/movies-search.component';
+import { SearchResultsComponent }     from './movies-search/search-results.component';
+import { MovieDetailsComponent }      from './movie-details/movie-details.component';
+
+// Services
 import { PopularService }             from './services/popular.service';
 import { NowPlayingService }          from './services/now-playing.service';
 import { TopRatedService }            from './services/top-rated.service';
 import { UpcomingService }            from './services/upcoming.service';
+import { SearchMoviesService }        from './services/search-movies.service';
+import { MovieDetailsService }        from './services/movie-details.service';
+
+// Pipes
+import { MovieTitleShortPipe }        from './pipes/movie-title-short.pipe';
+import { SafeUrlPipe }                from './pipes/safe-url.pipe';
 
 @NgModule({
   imports: [
@@ -37,31 +38,28 @@ import { UpcomingService }            from './services/upcoming.service';
   ],
   declarations: [
     MoviesComponent,
-    MoviesSearchComponent,
-    SearchResultsComponent,
-    MovieDetailsComponent,
-
-    // Refactoring
     DiscoverComponent,
     NowPlayingComponent,
     PopularComponent,
     TopRatedComponent,
     UpcomingComponent,
+    MoviesSearchComponent,
+    SearchResultsComponent,
+    MovieDetailsComponent,
 
+    // Pipes
     MovieTitleShortPipe,
     SafeUrlPipe
   ],
   providers: [
-    SearchMoviesService,
-    MovieDetailsService,
-    Location,
-
-    // Refactoring
-    DiscoverService,
     PopularService,
     NowPlayingService,
     TopRatedService,
-    UpcomingService
+    UpcomingService,
+    SearchMoviesService,
+    MovieDetailsService,
+    
+    Location,
   ]
 })
 export class MoviesModule { }
