81fa13f9787a64ba5cf86def3d8d874596c092c6
Tom <tgoralczyk1@gmail.com>
2017-01-25 09:54:16 -0500

    Refactored the search service and added the ability to preserve search queries

--- client/src/app/movies/services/search/search.service.ts
+++ client/src/app/movies/services/search/search.service.ts
@@ -9,15 +9,21 @@ import { API }            from '../moviedb-api-info';
 @Injectable()
 export class SearchService extends TMDBService {
   private apiUrl = API.url + '/search/movie?' + API.key + '&language=en-US&page=1&include_adult=false';
-
+  private cachedSearchQuery: string;
   constructor(private http: Http) { super(); }
 
   public search(title: string): Observable<Movie[]> {
+    this.cachedSearchQuery = title;
     title = title.replace(/\s/g, '%20');
     this.apiUrl += '&query=' + title;
+
     return this.http.get(this.apiUrl)
       .map(data => this.extractData(data))
       .catch(err => this.handleError(err));
+  }
+
+  public cachedQuery(): string {
+    return this.cachedSearchQuery || '';
   }
 
   private extractData(res: Response) {
