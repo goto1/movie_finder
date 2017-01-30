525874bd303502c8e020642a1f80e13eb4fc184b
Tom <tgoralczyk1@gmail.com>
2017-01-30 11:09:19 -0500

    Deleted unnecessary boilerplate files and adjusted the app.js file

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -11,12 +10,6 @@ const search = require('./routes/search');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -36,13 +29,16 @@ app.use((req, res, next) => {
 
 // error handler
 app.use((err, req, res, next) => {
+  const errDetailed = {
+    status: err.status || 500,
+    message: err.message,
+  };
+
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(errDetailed.status).send(errDetailed);
 });
 
 module.exports = app;
