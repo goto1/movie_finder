3a5281212ba03735eedcdf378bebafcad665e5ef
Tom <tgoralczyk1@gmail.com>
2017-01-29 19:32:29 -0500

    Created a new route for getting movie details

---
+++ server/routes/movie.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const router = express.Router();
+const rp = require('request-promise');
+const api = require('./api-info');
+const tmdb = require('./tmdb-helper');
+
+router.get('/:id', (req, res, next) => {
+
+  const url = api.url + '/movie/' +
+    req.params.id + '?' + api.key +
+    '&language=en-US&append_to_response=videos,similar';
+
+  rp({
+    uri: url,
+    json: true
+  })
+  .then(data => {
+    data = tmdb.extractMovieData(data);
+    res.json(data);
+  })
+  .catch(err => {
+    err = tmdb.handleError(err);
+    res.json(err);
+  });
+});
+
+module.exports = router;
