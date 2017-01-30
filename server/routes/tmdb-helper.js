50b2b115642bb4598633204d9133855af9c9e315
Tom <tgoralczyk1@gmail.com>
2017-01-30 06:53:07 -0500

    Added additional functionality for handling movie detail extraction

--- server/routes/tmdb-helper.js
+++ server/routes/tmdb-helper.js
@@ -4,6 +4,14 @@
 
 module.exports = {
 
+  /**
+   * Used by following routes:
+   *  -> /movies/now_playing/
+   *  -> /movies/upcoming/
+   *  -> /movies/top_rated/
+   *  -> /movies/popular/
+   *  -> /search/movie
+   */
   extractData(response) {
     const data = {};
 
@@ -17,20 +25,20 @@ module.exports = {
 
   extractMovies(data) {
     let movies = [];
-    const attributesToExtract = [
+    const attributes = [
       'id', 'poster_path',
       'title', 'vote_average',
     ];
 
     if (data) {
       movies = data.results.map(movie => {
-        const copy = {};
-        attributesToExtract.map(attribute => {
-          if (movie[attribute]) {
-            copy[attribute] = movie[attribute];
+        const temp = {};
+        attributes.map(attribute => {
+          if (movie.hasOwnProperty(attribute)) {
+            temp[attribute] = movie[attribute];
           }
         });
-        return copy;
+        return temp;
       });
     }
 
@@ -51,12 +59,63 @@ module.exports = {
     return data;
   },
 
+  /**
+   * Used by following routes:
+   *  -> /movie/:id
+   */
+  extractMovieData(response) {
+    let data = response || {};
+
+    data = this.extractAttributes(data);
+    data = this.extractImages(data);
+    data.similar = this.extractMovies(data.similar);
+    data.similar = this.extractPosterUrl(data.similar);
+
+    return data;
+  },
+
+  extractAttributes(movie) {
+    const attributes = [
+      'id', 'poster_path', 'title', 'backdrop_path',
+      'vote_average', 'imdb_id', 'overview', 'release_date',
+      'runtime', 'similar', 'trailer', 'vote_count', 'genres',
+    ];
+    const extracted = {};
+
+    if (movie) {
+      attributes.map(att => {
+        if (movie.hasOwnProperty(att)) {
+          extracted[att] = movie[att];
+        }
+        return extracted;
+      });
+    }
+
+    return extracted;
+  },
+
+  extractImages(data) {
+    const movie = data || {};
+
+    if (movie) {
+      movie.poster_path =
+        movie.poster_path ? 'https://image.tmdb.org/t/p/w500' + movie.poster_path : '';
+      movie.backdrop_path =
+        movie.backdrop_path ? 'https://image.tmdb.org/t/p/w1000' + movie.backdrop_path : '';
+    }
+
+    return movie;
+  },
+
+  /**
+   * Used for extracting the error message
+   */
   handleError(err) {
     const details = {};
 
     if (err) {
       details.status_code = err.response.body.status_code || 400;
-      details.status_message = err.response.body.status_message || 'Could not get any response';
+      details.status_message = err.response.body.status_message || 'Not found';
     }
 
     return details;
