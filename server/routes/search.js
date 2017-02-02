72084c4bac3763e2f2f5db9040803671b0d45283
Tom <tgoralczyk1@gmail.com>
2017-02-02 07:58:46 -0500

    Minor changes and code refactoring

--- server/routes/search.js
+++ server/routes/search.js
@@ -7,31 +7,31 @@ const request = require('request-promise');
 const options = {
   method: 'GET',
   json: true,
+  uri: `${api.url}/search/movie`,
   qs: {
     api_key: api.key,
     language: 'en-US',
-    page: 1,
   },
 };
 
 router.get('/movie', (req, res, next) => {
-  if (!req.query.q) {
+  if (!req.query.query) {
     res.status(400).send({
       status_code: 400,
       status_message: 'Bad Request',
-    });
+    }).end();
   } else {
-    options.qs.query = req.query.q;
-    options.uri = `${api.url}/search/movie`;
+    options.qs.query = req.query.query;
+    options.qs.page = req.query.page || 1;
 
     request(options)
     .then((data) => {
-      data = tmdb.extractData(data);
-      res.json(data);
+      const movies = tmdb.extractData(data);
+      res.json(movies);
     })
     .catch((err) => {
-      err = tmdb.handleError(err);
-      res.status(400).send(err);
+      const error = tmdb.handleError(err);
+      res.status(400).send(error).end();
     });
   }
 });
