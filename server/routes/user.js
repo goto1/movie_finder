5d6153487b93fd88fb0c1aee1a4c695eaf6ea991
Tom <tgoralczyk1@gmail.com>
2017-02-04 06:58:37 -0500

    Added a route for updating user information and deleting a user

--- server/routes/user.js
+++ server/routes/user.js
@@ -18,11 +18,26 @@ router.route('/')
 // --> /user/:id
 router.route('/:id')
   .get((req, res, next) => {
-
-    console.log(req.params.id);
-
     User.findById(req.params.id).exec()
       .then((response) => res.json(response))
+      .catch((err) => res.send(err));
+  })
+  .put((req, res, next) => {
+    const options = {
+      password: req.body.password,
+      email: req.body.email,
+      updatedAt: Date.now(),
+    };
+
+    console.log(Date.now());
+
+    User.findByIdAndUpdate(req.params.id, options).exec()
+      .then((response => res.json({ message: 'User info successfully updated!' })))
+      .catch((err) => res.send({ error: err }));
+  })
+  .delete((req, res, next) => {
+    User.findByIdAndRemove(req.params.id).exec()
+      .then((response) => res.json({ message: 'User successfully deleted!' }))
       .catch((err) => res.send(err));
   });
 
