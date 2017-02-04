2fd6384edc5ea4777b0a74951df7bb24e8a5c127
Tom <tgoralczyk1@gmail.com>
2017-02-04 06:12:02 -0500

    Minor changes to the schema, added validation

--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,26 @@
-const db = require('../secret/db');
+const mongoose = require('mongoose');
 
-const userSchema = db.Schema({
-  username: String,
-  password: String,
-  email: String,
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'Username is required'],
+  },
+  password: {
+    type: String,
+    minlength: [8, 'Password should be at least 8 characters'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-const User = db.model('User', userSchema);
-
-module.exports = User;
+module.exports = mongoose.model('user', userSchema);
