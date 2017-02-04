b0476f757652fb93ceefdc060d5239eea29fb628
Tom <tgoralczyk1@gmail.com>
2017-02-04 06:48:16 -0500

    Added a route for getting information about the user from the database

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/user');
 
+// --> /user/
 router.route('/')
   .post((req, res, next) => {
     const user = new User({
@@ -11,6 +12,17 @@ router.route('/')
 
     user.save()
       .then((response) => res.json({ message: 'New user added! ' }))
+      .catch((err) => res.send(err));
+  });
+
+// --> /user/:id
+router.route('/:id')
+  .get((req, res, next) => {
+
+    console.log(req.params.id);
+
+    User.findById(req.params.id).exec()
+      .then((response) => res.json(response))
       .catch((err) => res.send(err));
   });
 
