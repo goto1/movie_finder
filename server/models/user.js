f0fcdb5772fc207c724205975257206a6ed56564
Tom <tgoralczyk1@gmail.com>
2017-02-05 07:44:36 -0500

    Got rid of username and using email as the username

--- server/models/user.js
+++ server/models/user.js
@@ -1,17 +1,14 @@
 const mongoose = require('mongoose');
+const Movie = require('./movie');
 
 const userSchema = new mongoose.Schema({
-  username: {
+  email: {
     type: String,
-    required: [true, 'Username is required'],
+    required: [true, 'Email is required'],
   },
   password: {
     type: String,
     minlength: [8, 'Password should be at least 8 characters'],
-  },
-  email: {
-    type: String,
-    required: [true, 'Email is required'],
   },
   createdAt: {
     type: Date,
