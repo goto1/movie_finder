93d5862af5a01c6bad0e0c12ffdece7d8c592c37
Tom <tgoralczyk1@gmail.com>
2017-02-05 07:43:39 -0500

    Added a registration route

--- server/routes/user.js
+++ server/routes/user.js
@@ -7,11 +7,11 @@ function hash(password) {
   return bcrypt.hash(password, saltRounds);
 }
 
-// --> /user/
-router.route('/')
+// --> /user/register
+router.route('/register')
   .post((req, res, next) => {
 
-    if (!req.body.email || !req.body.username || !req.body.password) {
+    if (!req.body.email || !req.body.password) {
       return res.status(400).send({
         status: 400,
         message: 'Missing required user information',
@@ -22,7 +22,6 @@ router.route('/')
       .then((hashed) => {
         const user = new User({
           email: req.body.email,
-          username: req.body.username,
           password: hashed,
         });
 
@@ -39,6 +38,43 @@ router.route('/')
       .catch(err => res.status(500).send({
         status: 500,
         message: 'Could not register a new account',
+      }));
+  });
+
+// /user/login
+router.route('/login')
+  .post((req, res, next) => {
+    if (!req.body.email || !req.body.password) {
+      return res.status(400).send({
+        status: 400,
+        message: 'Missing required user information',
+      });
+    }
+
+    User.where({ email: req.body.email }).findOne()
+      .then((user) => {
+        bcrypt.compare(req.body.password, user.password)
+          .then((matched) => {
+            if (!matched) {
+              return res.status(400).send({
+                status: 200,
+                message: 'User email or password is incorrect',
+              });
+            }
+
+            return res.status(200).send({
+              status: 200,
+              message: 'User successfully authenticated',
+            });
+          })
+          .catch((err) => res.status(500).send({
+            status: 500,
+            message: 'Something went wrong',
+          }));
+      })
+      .catch((err) => res.status(200).send({
+        status: 400,
+        message: 'Could not find the user with specified information',
       }));
   });
 
