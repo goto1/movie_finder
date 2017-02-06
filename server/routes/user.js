aa95d45d2683dac159b6fff99157f2258e0c9fea
Tom <tgoralczyk1@gmail.com>
2017-02-06 06:34:11 -0500

    Code refactoring

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,105 +1,100 @@
-const router = require('express').Router();
-const User = require('../models/user');
-const bcrypt = require('bcrypt');
+const express = require('express');
+const mongoose = require('mongoose');
+const { MovieSchema, UserSchema } = require('../models/schemas');
 
-function hash(password) {
-  const saltRounds = 10;
-  return bcrypt.hash(password, saltRounds);
+const router = express.Router();
+const Movie = mongoose.model('Movie', MovieSchema);
+const User = mongoose.model('User', UserSchema);
+
+// /user/:id/movie/:id/favorites
+// /user/:id/movie/:id/bookmarked
+// /user/:id/favorites/:id
+// /user/:id/bookmark/:id
+
+function createResponseMessage(status, message) {
+  return { status, message };
 }
 
-// --> /user/register
-router.route('/register')
-  .post((req, res, next) => {
+// -> /user/register
+router.post('/register', (req, res, next) => {
+  if (!req.body.email || !req.body.password) {
+    return res.status(400)
+      .send(createResponseMessage(400, 'Missing required information'));
+  }
 
-    if (!req.body.email || !req.body.password) {
-      return res.status(400).send({
-        status: 400,
-        message: 'Missing required user information',
-      });
-    }
-
-    hash(req.body.password)
-      .then((hashed) => {
-        const user = new User({
-          email: req.body.email,
-          password: hashed,
-        });
-
-        user.save()
-          .then(response => res.status(200).send({
-            status: 200,
-            message: 'Account created successfully',
-          }))
-          .catch(err => res.status(500).send({
-            status: 500,
-            message: 'Could not register a new account',
-          }));
-      })
-      .catch(err => res.status(500).send({
-        status: 500,
-        message: 'Could not register a new account',
-      }));
+  const user = new User({
+    email: req.body.email,
+    password: req.body.password,
   });
 
-// /user/login
-router.route('/login')
-  .post((req, res, next) => {
-    if (!req.body.email || !req.body.password) {
-      return res.status(400).send({
-        status: 400,
-        message: 'Missing required user information',
-      });
-    }
+  user.save()
+    .then(response => res.send(createResponseMessage(200, 'User created successfully')))
+    .catch((err) => {
+      const message = err.message || 'Could not create a new user';
+      return res.status(500).send(createResponseMessage(500, message));
+    });
+});
 
-    User.where({ email: req.body.email }).findOne()
-      .then((user) => {
-        bcrypt.compare(req.body.password, user.password)
-          .then((matched) => {
-            if (!matched) {
-              return res.status(400).send({
-                status: 200,
-                message: 'User email or password is incorrect',
-              });
-            }
-
-            return res.status(200).send({
-              status: 200,
-              message: 'User successfully authenticated',
-            });
-          })
-          .catch((err) => res.status(500).send({
-            status: 500,
-            message: 'Something went wrong',
-          }));
-      })
-      .catch((err) => res.status(200).send({
-        status: 400,
-        message: 'Could not find the user with specified information',
-      }));
-  });
-
-// --> /user/:id
-router.route('/:id')
-  .get((req, res, next) => {
-    User.findById(req.params.id).exec()
-      .then((response) => res.json(response))
-      .catch((err) => res.send(err));
-  })
-  .put((req, res, next) => {
-    const options = {
-      password: req.body.password,
-      email: req.body.email,
-      updatedAt: Date.now(),
-    };
-
-    User.findByIdAndUpdate(req.params.id, options).exec()
-      .then((response => res.json({ message: 'User info successfully updated!' })))
-      .catch((err) => res.send({ error: err }));
-  })
-  .delete((req, res, next) => {
-    User.findByIdAndRemove(req.params.id).exec()
-      .then((response) => res.json({ message: 'User successfully deleted!' }))
-      .catch((err) => res.send(err));
-  });
+//
+// // /user/login
+// router.route('/login')
+//   .post((req, res, next) => {
+//     if (!req.body.email || !req.body.password) {
+//       return res.status(400).send({
+//         status: 400,
+//         message: 'Missing required user information',
+//       });
+//     }
+//
+//     User.where({ email: req.body.email }).findOne()
+//       .then((user) => {
+//         bcrypt.compare(req.body.password, user.password)
+//           .then((matched) => {
+//             if (!matched) {
+//               return res.status(400).send({
+//                 status: 200,
+//                 message: 'User email or password is incorrect',
+//               });
+//             }
+//
+//             return res.status(200).send({
+//               status: 200,
+//               message: 'User successfully authenticated',
+//             });
+//           })
+//           .catch((err) => res.status(500).send({
+//             status: 500,
+//             message: 'Something went wrong',
+//           }));
+//       })
+//       .catch((err) => res.status(200).send({
+//         status: 400,
+//         message: 'Could not find the user with specified information',
+//       }));
+//   });
+//
+// // --> /user/:id
+// router.route('/:id')
+//   .get((req, res, next) => {
+//     User.findById(req.params.id).exec()
+//       .then((response) => res.json(response))
+//       .catch((err) => res.send(err));
+//   })
+//   .put((req, res, next) => {
+//     const options = {
+//       password: req.body.password,
+//       email: req.body.email,
+//       updatedAt: Date.now(),
+//     };
+//
+//     User.findByIdAndUpdate(req.params.id, options).exec()
+//       .then((response => res.json({ message: 'User info successfully updated!' })))
+//       .catch((err) => res.send({ error: err }));
+//   })
+//   .delete((req, res, next) => {
+//     User.findByIdAndRemove(req.params.id).exec()
+//       .then((response) => res.json({ message: 'User successfully deleted!' }))
+//       .catch((err) => res.send(err));
+//   });
 
 module.exports = router;
