a09989c2b80092b202f4603c45af9fcf3ecfa842
Tom <tgoralczyk1@gmail.com>
2017-02-09 11:32:37 -0500

    Implemented a new schema for the user

---
+++ server/models/users.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const secret = require('../secret/jwt');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  hash: String,
+  salt: String,
+});
+
+userSchema.methods.setPassword = (password) => {
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha1').toString('hex');
+};
+
+userSchema.methods.validPassword = (password) => {
+  const hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha1').toString('hex');
+  return this.hash === hash;
+};
+
+userSchema.methods.generateJwt = () => {
+  const expiry = new Date();
+  expiry.setDate(expiry.getDate() + 7);
+
+  return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    name: this.name,
+    exp: parseInt(expiry.getTime() / 1000)
+  }, secret);
+};
+
+mongoose.model('User', userSchema);
