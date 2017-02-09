24b7c43fd72802ae140493697d3b3ec4e7cd5276
Tom <tgoralczyk1@gmail.com>
2017-02-09 11:33:08 -0500

    Implemented passport to handle authentication

--- server/app.js
+++ server/app.js
@@ -3,12 +3,12 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
+const passport = require('passport');
 
-require('./configs/database');
+require('./secret/db');
+require('./config/passport');
 
-const movie = require('./routes/movie');
-const search = require('./routes/search');
-const user = require('./routes/user');
+const routes = require('./routes/index');
 
 const app = express();
 
@@ -18,9 +18,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use('/movie', movie);
-app.use('/search', search);
-app.use('/user', user);
+app.use(passport.initialize());
+app.use('/api', routes);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
---
+++ server/config/passport.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const User = mongoose.model('User');
+
+passport.use(new LocalStrategy({ usernameField: 'email' },
+  (username, password, done) => {
+    User.findOne({ email: username }).exec()
+      .then((user) => {
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username' });
+        }
+
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password' });
+        }
+
+        return done(null, user);
+      })
+      .catch(err => done(err));
+  }));
