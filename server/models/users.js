641fcb5dd50ec7ab299ab97b47c7f6527484a9ec
Tom <tgoralczyk1@gmail.com>
2017-02-11 18:49:52 -0500

    Implemented bcrypt for hashing passwords

--- server/models/users.js
+++ server/models/users.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const secret = require('../secret/jwt');
+const rounds = require('../secret/salt');
 
 const Schema = mongoose.Schema;
 
@@ -20,14 +22,32 @@ const userSchema = new Schema({
 });
 
 userSchema.methods.setPassword = (password) => {
-  this.salt = crypto.randomBytes(16).toString('hex');
-  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha1').toString('hex');
-};
+  bcrypt.hash(password, rounds)
+    .then(hash => {
+      this.hash = hash;
+    })
+    .catch(err => console.log(err));
+}
 
-userSchema.methods.validPassword = (password) => {
-  const hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha1').toString('hex');
-  return this.hash === hash;
-};
+userSchema.methods.validatePassword = (password) => {
+  return bcrypt.hash(password, rounds)
+    .then(hash => {
+      bcrypt.compare(password, hash)
+        .then(matched => true)
+        .catch(err => false);
+    })
+    .catch(err => false);
+}
+
+// userSchema.methods.setPassword = (password) => {
+//   this.salt = crypto.randomBytes(16).toString('hex');
+//   this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha1').toString('hex');
+// };
+
+// userSchema.methods.validPassword = (password) => {
+//   const hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha1').toString('hex');
+//   return this.hash === hash;
+// };
 
 userSchema.methods.generateJwt = () => {
   const expiry = new Date();
