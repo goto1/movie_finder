fa210359b448eb8018ffe19fe20d28ae897b0e06
Tom <tgoralczyk1@gmail.com>
2017-02-15 07:56:58 -0500

    Code refactoring

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,24 +1,16 @@
-const mongoose = require('mongoose');
+const User = require('mongoose').model('User');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-
-const User = mongoose.model('User');
 
 passport.use(new LocalStrategy({ usernameField: 'email' },
   (username, password, done) => {
     User.findOne({ email: username }).exec()
       .then((user) => {
-        if (!user) {
-          console.log('1', user);
-          return done(null, false, { message: 'Incorrect username' });
+        if (!user || !user.validatePassword(password)) {
+          return done(null, false, {
+            message: 'Incorrect username or password'
+          });
         }
-
-        if (!user.validatePassword(password)) {
-          console.log(user.validatePassword(password));
-          console.log('2', user);
-          return done(null, false, { message: 'Incorrect password' });
-        }
-
         return done(null, user);
       })
       .catch(err => done(err));
