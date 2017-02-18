ddfcd2ebbffd9b3974a40fafb0fc487e8c25d47e
Tom <tgoralczyk1@gmail.com>
2017-02-18 13:30:06 -0500

    Code factoring in the generateJwt method and hashed the password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -1,57 +1,38 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const validator = require('validator');
-const xssFilters = require('xss-filters');
-const config = require('../secret/main');
+const config = require('../secret/config');
+const sanitizer = require('../helpers/sanitizer');
 
 const Schema = mongoose.Schema;
 
-const setFirstName = fname => xssFilters.inHTMLData(fname);
-const setLastName = lname => xssFilters.inHTMLData(lname);
-const setEmail =
-  (email) => {
-    let clean = email;
-    clean = xssFilters.inHTMLData(clean);
-    clean = validator.trim(clean);
-    clean = validator.escape(clean);
-    clean = validator.normalizeEmail(clean);
-
-    return clean;
-  };
-
 const userSchema = new Schema({
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    set: setEmail,
-  },
-  first_name: { type: String, required: true, set: setFirstName },
-  last_name: { type: String, required: true, set: setLastName },
+  email: { type: String, unique: true, required: true, set: sanitizer.email },
   password: { type: String, required: true },
+  first_name: { type: String, required: true, set: sanitizer.name },
+  last_name: { type: String, required: true, set: sanitizer.name },
 });
 
-userSchema.methods.validatePassword =
-  password =>
-    bcrypt.hash(password, config.rounds)
-      .then((hash) => {
-        bcrypt.compare(password, hash)
-          .then(() => true)
-          .catch(() => false);
-      })
-      .catch(() => false);
+userSchema.pre('save', function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  bcrypt.hash(this.password, config.salt_rounds)
+    .then((hash) => {
+      this.password = hash;
+      next();
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 userSchema.methods.generateJwt = () => {
-  const expiry = new Date();
-  expiry.setDate(expiry.getDate() + 7);
-
   return jwt.sign({
     _id: this._id,
     email: this.email,
-    name: this.name,
-    exp: parseInt(expiry.getTime() / 1000)
-  }, config.secret);
+  }, config.secret, { expiresIn: '1h' });
 };
 
 mongoose.model('User', userSchema);
