abe5b5cc53133c571adbc67f228eaa2efc420f3c
Tom <tgoralczyk1@gmail.com>
2017-02-19 08:25:45 -0500

    Implemented a new strategy for route authorization

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,17 +1,33 @@
 const User = require('mongoose').model('User');
 const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
 const LocalStrategy = require('passport-local').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const secret = require('../secret/config').secret;
 
 passport.use(new LocalStrategy({ usernameField: 'email' },
   (username, password, done) => {
     User.findOne({ email: username }).exec()
       .then((user) => {
         if (!user || !user.validatePassword(password)) {
-          return done(null, false, {
-            message: 'Incorrect username or password'
-          });
+          return done(null, false, { message: 'Incorrect username or password' });
         }
         return done(null, user);
       })
       .catch(err => done(err));
   }));
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeader(),
+  secretOrKey: secret,
+};
+
+passport.use(new JwtStrategy(jwtOptions, (jwt_payload, next) => {
+  User.findOne({ _id: jwt_payload.id }).exec()
+    .then((user) => {
+      next(null, user);
+    })
+    .catch(() => {
+      next(null, false);
+    });
+}));
