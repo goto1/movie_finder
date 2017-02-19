760baea306296eff64dda5fea424f5c862387d0e
Tom <tgoralczyk1@gmail.com>
2017-02-19 08:56:19 -0500

    Minor changes in the routes to work with new passport strategies

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,15 +1,8 @@
 const router = require('express').Router();
-const jwt = require('express-jwt');
-const secret = require('../secret/jwt');
 const userController = require('../controllers/user');
 const authController = require('../controllers/authentication');
 
-const auth = jwt({
-  secret,
-  userProperty: 'payload',
-});
-
-router.get('/profile', auth, userController.getProfile);
+router.get('/profile', userController.getProfile);
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
