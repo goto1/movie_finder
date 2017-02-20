42a13a0456ed1070e01311cea02566a5f016ebb4
Tom <tgoralczyk1@gmail.com>
2017-02-20 16:37:13 -0500

    Minor code refactoring

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,15 +1,16 @@
-const mongoose = require('mongoose');
 const passport = require('passport');
+const resJSON = require('../helpers/response');
 
-module.exports.getProfile = (req, res, next) => {
+module.exports.getProfile = (req, res) => {
   passport.authenticate('jwt', { session: false },
     (err, user) => {
       if (err) {
-        return res.status(404).json({
-          message: 'Something went wrong'
-        });
+        return resJSON.internalServerError(res, 'Something went wrong');
       }
 
-      return res.status(200).json(user);
+      return res.status(200).json({
+        status: 200,
+        result: user,
+      });
     })(req, res);
 };
