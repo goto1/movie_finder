21b2b47edce471aaa97c09bd015417e43de26c51
Tom <tgoralczyk1@gmail.com>
2017-02-20 08:25:41 -0500

    Added the routes for handling movies

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,15 @@
 const router = require('express').Router();
 const userController = require('../controllers/user');
 const authController = require('../controllers/authentication');
+const movieController = require('../controllers/movie');
 
 router.get('/profile', userController.getProfile);
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+
+router.get('/movie', movieController.getMovies);
+router.post('/movie', movieController.addMovie);
+router.delete('/movie', movieController.deleteMovie);
 
 module.exports = router;
