4ada4c544ffec5a34421af30063f279e6c43f2a2
Tom <tgoralczyk1@gmail.com>
2017-02-21 07:51:00 -0500

    Added a helper for handling API request for the TMDB API

---
+++ server/helpers/tmdb.js
@@ -0,0 +1,30 @@
+const request = require('request-promise');
+const api = require('../secret/tmdb_api');
+
+const options = {
+  method: 'GET',
+  json: true,
+  qs: {
+    uri: `${api.url}/movie`,
+    api_key: api.key,
+    language: 'en-US',
+  },
+};
+
+module.exports = {
+  extractMovie(data) {
+    return {
+      id: data.id,
+      title: data.title,
+      vote_average: data.vote_average,
+      genres: data.genres,
+      poster_path: data.poster_path ? `https://image.tmdb.org/t/p/w500${data.poster_path}` : '',
+    };
+  },
+
+  getMovie(id) {
+    options.url = `${options.qs.uri}/${id}`;
+
+    return request(options);
+  },
+};
