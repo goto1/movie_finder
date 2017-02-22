329f27afa0df5d855e2e24e81014a03f578148b8
Tom <tgoralczyk1@gmail.com>
2017-02-22 07:14:53 -0500

    Added the user module

---
+++ client/src/app/user/user-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { LoginComponent } from './login/login.component';
+
+const userRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+];
+
+@NgModule({
+  imports: [ RouterModule.forChild(userRoutes) ],
+  exports: [ RouterModule ]
+})
+export class UserRoutingModule { }
---
+++ client/src/app/user/user.module.ts
@@ -0,0 +1,18 @@
+import { NgModule }           from '@angular/core';
+import { CommonModule }       from '@angular/common';
+
+import { UserRoutingModule }  from './user-routing.module';
+
+import { LoginComponent }     from './login/login.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    UserRoutingModule
+  ],
+  declarations: [
+    LoginComponent
+  ],
+  providers: [],
+})
+export class UserModule { }
