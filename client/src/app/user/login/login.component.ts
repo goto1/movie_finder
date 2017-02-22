ff80399d8404c17bb0f0585e0f66d96be53796b5
Tom <tgoralczyk1@gmail.com>
2017-02-22 11:37:23 -0500

    Implemented form builder and added some validation

--- client/src/app/user/login/login.component.ts
+++ client/src/app/user/login/login.component.ts
@@ -1,7 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './login.component.html',
   styleUrls: [ './login.component.sass' ]
 })
-export class LoginComponent { }
+export class LoginComponent implements OnInit { 
+  public user: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.user = this.fb.group({
+      email: ['', [Validators.required, Validators.minLength(2)]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+  }
+
+  public onSubmit(): void {
+    console.log(this.user.value);
+  }
+}
