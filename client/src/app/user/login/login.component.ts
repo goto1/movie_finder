35db186be122d8800819eade75c92b6f4c6af547
Tomasz Goralczyk <devflag@gmail.com>
2017-02-25 11:26:09 -0500

    Implemented the login service into the login component so that users can log in

--- client/src/app/user/login/login.component.ts
+++ client/src/app/user/login/login.component.ts
@@ -2,24 +2,47 @@ import { Component, OnInit }  from '@angular/core';
 import { 
   FormBuilder, FormControl, 
   FormGroup, Validators }     from '@angular/forms';
+import { Router }             from '@angular/router';
+import { LoginService }       from '../services/login.service';
 
 @Component({
   templateUrl: './login.component.html',
   styleUrls: [ '../form-styles.sass' ]
 })
 export class LoginComponent implements OnInit { 
-  public user: FormGroup;
+  user: FormGroup;
+  error: string;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private ls: LoginService ) { }
 
   ngOnInit(): void {
     this.user = this.fb.group({
       email: ['', [Validators.required]],
       password: ['', [Validators.required]]
     });
+
+    this.ls.logout();
   }
 
   public onSubmit(): void {
-    console.log(this.user.value);
+    let email = this.user.value.email;
+    let password = this.user.value.password;
+
+    this.ls.login(email, password)
+      .subscribe(
+        result => {
+          if (result === true) {
+            this.router.navigate(['/']);
+          } else {
+            this.error = 'Email or password is incorrect';
+          }
+        },
+        err => {
+          let details = err.json() || {};
+          this.error = details.message;
+        });
   }
 }
