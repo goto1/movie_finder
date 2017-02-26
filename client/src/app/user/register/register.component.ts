b79143228d47b6a3b287629d9459e2039167f532
Tomasz Goralczyk <devflag@gmail.com>
2017-02-26 10:07:05 -0500

    Implemented the registration functionality

--- client/src/app/user/register/register.component.ts
+++ client/src/app/user/register/register.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit }  from '@angular/core';
+import { Component, OnInit }      from '@angular/core';
 import {
   FormBuilder, FormControl, 
-  FormGroup, Validators }     from '@angular/forms';
+  FormGroup, Validators }         from '@angular/forms';
+import { Router }                 from '@angular/router';
+import { AuthenticationService }  from '../services/authentication.service';
+import { IRegisterForm }          from '../../shared/interfaces';
 
 @Component({
   templateUrl: './register.component.html',
   styleUrls: [ '../form-styles.sass' ]
 })
 export class RegisterComponent implements OnInit {
-  public user: FormGroup;
+  user: FormGroup;
+  error: string;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private auth: AuthenticationService ) { }
 
   ngOnInit(): void {
     this.user = this.fb.group({
@@ -20,13 +27,39 @@ export class RegisterComponent implements OnInit {
       password: ['', [Validators.required, Validators.minLength(10)]],
       passwordRepeated: ['', [Validators.required, Validators.minLength(8)]]
     });
+
+    this.auth.logout(); // just to be safe
   }
 
-  public onSubmit(): void {
-    console.log(this.user.value);
+  onSubmit(): void {
+    const formData: IRegisterForm = {
+      first_name: this.user.value.firstName,
+      last_name: this.user.value.lastName,
+      email: this.user.value.email,
+      password: this.user.value.password
+    };
+
+    this.error = '';
+
+    this.register(formData);
   }
 
-  public passwordMatching(): boolean {
+  private register(formData: IRegisterForm): void {
+    this.auth.register(formData)
+      .subscribe(
+        result => {
+          if (!result) {
+            this.error = 'Registration failed, try again';
+            return;
+          }
+          this.router.navigate(['/']);
+        },
+        err => {
+          this.error = err;
+        });
+  }
+
+  passwordsMatching(): boolean {
     return this.user.value.password === this.user.value.passwordRepeated;
   }
 }
