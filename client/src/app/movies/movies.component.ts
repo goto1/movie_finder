785beb3073fa2957a97a7facbb266b882e8e2bc1
Tomasz Goralczyk <devflag@gmail.com>
2017-03-02 06:37:04 -0500

    Added a link to recommendations page to be shown when user is logged in

--- client/src/app/movies/movies.component.ts
+++ client/src/app/movies/movies.component.ts
@@ -1,5 +1,7 @@
 import { Component }  from '@angular/core';
 
+import { AuthenticationService } from '../user/services/authentication.service';
+
 import '../shared/rxjs-operators';
 
 @Component({
@@ -11,9 +13,18 @@ import '../shared/rxjs-operators';
       <button class="btn btn-nav" routerLink="/search" routerLinkActive="active">
         Search <i class="fa fa-search" aria-hidden="true"></i>
       </button>
+      <button 
+        class="btn btn-nav" 
+        routerLink="/recommended" 
+        routerLinkActive="active"
+        *ngIf="auth.loggedIn()">
+        Recommended <i class="fa fa-heart-o" aria-hidden="true"></i>
+      </button>
     </nav>
     <router-outlet></router-outlet>
   `,
   styleUrls: [ './movies.component.sass' ]
 })
-export class MoviesComponent { }
+export class MoviesComponent {
+  constructor(private auth: AuthenticationService) { }
+}
