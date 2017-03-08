f6e11f26e4802c263cd1bc708288d0b815b65d90
Tomasz Goralczyk <devflag@gmail.com>
2017-03-08 08:23:23 -0500

    Added a pipe for sanatizing urls

---
+++ client/src/app/pipes/sanitize-url.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
+@Pipe({ name: 'sanitizeUrl' })
+export class SanitizeUrlPipe implements PipeTransform{
+  constructor(private sanitizer: DomSanitizer) { }
+
+  transform(url: string) {
+    return this.sanitizer.bypassSecurityTrustUrl(url);
+  }
+}
