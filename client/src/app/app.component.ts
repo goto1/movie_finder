dd725ea301e27d8161c89453353b0b765bb802d9
Tomasz Goralczyk <devflag@gmail.com>
2017-03-10 09:16:56 -0500

    Moved the main navigation into the app component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -19,6 +19,27 @@ import { AuthenticationService }  from './user/services/authentication.service';
         </button>
       </nav>
     </header>
+    <nav class="main-nav">
+      <button 
+        class="btn btn-nav"
+        routerLink="/discover" 
+        routerLinkActive="btn-nav-active">
+        Discover <i class="fa fa-film" aria-hidden="true"></i>
+      </button>
+      <button 
+        class="btn btn-nav"
+        routerLink="/search" 
+        routerLinkActive="btn-nav-active">
+        Search <i class="fa fa-search" aria-hidden="true"></i>
+      </button>
+      <button
+        class="btn btn-nav"
+        routerLink="/recommended"
+        routerLinkActive="btn-nav-active"
+        *ngIf="auth.loggedIn()">
+        Recommended <i class="fa fa-heart-o" aria-hidden="true"></i>
+      </button>
+    </nav>
     <router-outlet></router-outlet>
   `,
   styleUrls: [ './app.component.sass' ]
