06162f43e6f8b9147149aa094334fab9c241ef78
Tomasz Goralczyk <devflag@gmail.com>
2017-03-11 12:57:36 -0500

    Implemented the shared module

--- client/src/app/modules/discover.module.ts
+++ client/src/app/modules/discover.module.ts
@@ -1,23 +1,21 @@
 import { NgModule } from '@angular/core';
-import { CommonModule, Location } from '@angular/common';
 import { HttpModule } from '@angular/http';
 
 import { DiscoverRoutingModule } from './discover-routing.module';
+import { SharedModule } from './shared.module';
 
 import { DiscoverMoviesComponent } from '../components/discover/discover-movies.component';
 import { NowPlayingMoviesComponent } from '../components/discover/now-playing-movies.component';
 import { PopularMoviesComponent } from '../components/discover/popular-movies.component';
 import { TopRatedMoviesComponent } from '../components/discover/top-rated-movies.component';
 import { UpcomingMoviesComponent } from '../components/discover/upcoming-movies.component';
-import { MovieItemComponent } from '../components/movie-item/movie-item.component';
-import { MovieListComponent } from '../components/movie-list/movie-list.component';
+
 import { MovieService } from '../services/movie.service';
-import { SanitizeUrlPipe } from '../pipes/sanitize-url.pipe'
 
 @NgModule({
   imports: [
-    CommonModule,
     HttpModule,
+    SharedModule,
     DiscoverRoutingModule
   ],
   declarations: [ 
@@ -25,14 +23,10 @@ import { SanitizeUrlPipe } from '../pipes/sanitize-url.pipe'
     NowPlayingMoviesComponent,
     PopularMoviesComponent,
     TopRatedMoviesComponent,
-    UpcomingMoviesComponent,
-    MovieItemComponent,
-    MovieListComponent,
-    SanitizeUrlPipe
+    UpcomingMoviesComponent
   ],
   providers: [
-    MovieService,
-    Location
+    MovieService
   ]
 })
 export class DiscoverModule { }
---
+++ client/src/app/modules/search.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MovieService } from '../services/movie.service';
+import { SharedModule } from '../modules/shared.module';
+
+import { SearchMoviesComponent } from '../components/search/search-movies.component';
+
+const searchRoutes: Routes = [
+  { path: 'search', component: SearchMoviesComponent }
+];
+
+@NgModule({
+  imports: [
+    HttpModule,
+    SharedModule,
+    RouterModule.forRoot(searchRoutes),
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  declarations: [
+    SearchMoviesComponent,
+  ],
+  providers: [ MovieService ]
+})
+export class SearchModule { }
