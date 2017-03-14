a47987670da8f2cd1a113d566612025815d10b9b
Tomasz Goralczyk <devflag@gmail.com>
2017-03-14 09:32:15 -0400

    Minor changes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,11 +3,8 @@ import { RouterModule, Routes }   from '@angular/router';
 
 import { PageNotFoundComponent }  from './page-not-found.component';
 
-// import { TestComponent } from './components/test.component';
-
 export const appRoutes: Routes = [
   { path: '', redirectTo: '/discover/now_playing', pathMatch: 'full' },
-  // { path: 'test', component: TestComponent } // DELETE AFTER DONE TESTING!!!
   //{ path: '**', component: PageNotFoundComponent }
 ];
 
