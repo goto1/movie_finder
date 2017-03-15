1323c297142ab8cc2a5b12e922c69d110b41dec3
Tomasz Goralczyk <devflag@gmail.com>
2017-03-15 07:03:57 -0400

    Added some logic to not show any recommendations unless the user liked a few movies

--- client/src/app/components/recommended/recommended-movies.component.ts
+++ client/src/app/components/recommended/recommended-movies.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 import { MovieService } from '../../services/movie.service';
+import { UserService } from '../../services/user.service';
 
 import { IMovieOverview } from '../../shared/interfaces';
 
 @Component({
   template: `
-    <div>
+    <div *ngIf="movies">
       <h1>Movies You Might Like...</h1>
       <movie-list [movies]="movies"></movie-list>
+    </div>
+    <div *ngIf="!movies" class="no-movies">
+      No recommendations to show. You need to <i class="fa fa-heart" aria-hidden="true"></i> a few movies first
+      before you're able to get recommendations.
     </div>
   `,
   styleUrls: [ './recommended-movies.component.sass' ]
@@ -16,15 +21,21 @@ import { IMovieOverview } from '../../shared/interfaces';
 export class RecommendedMoviesComponent implements OnInit {
   movies: IMovieOverview[];
 
-  constructor(private movieService: MovieService) { }
+  constructor(
+    private movieService: MovieService,
+    private userService: UserService ) { }
 
   ngOnInit(): void {
+    this.userService.getMovies().subscribe();
+
     const userInfo = JSON.parse(localStorage.getItem('user_info'));
 
-    this.movieService.getMoviesWithGenres(userInfo.genreIds)
-      .subscribe(
-        res => { this.movies = res.movies },
-        err => console.error(err)
-      );
+    if (userInfo.genreIds.length > 0) {
+      this.movieService.getMoviesWithGenres(userInfo.genreIds)
+        .subscribe(
+          res => { this.movies = res.movies; console.log(res.movies); },
+          err => console.error(err)
+        );
+    }
   }
 }
